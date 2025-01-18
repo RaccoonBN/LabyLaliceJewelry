68a114efbb9ee1e7090f3ae68c4ceb9ddc8972c4
Nduy <168271459+gsggsfsasdsdasd@users.noreply.github.com>
2025-01-18 23:07:16 +0700

    Update App.js

--- Client/frontend/src/App.js
+++ Client/frontend/src/App.js
@@ -5,6 +5,10 @@ import Header from "./components/header";
 import Footer from "./components/footer"; 
 import OrderHistory from "./pages/OrderHistory"; 
 import EditAccount from "./pages/EditAccount"; 
+import AboutUs from "./pages/AboutUs";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -16,6 +20,11 @@ function App() {
           <Route path="/" element={<Home />} /> 
           <Route path="/order-history" element={<OrderHistory />} /> {/* Route cho Order History */}
           <Route path="/edit-account" element={<EditAccount />} /> {/* Route cho Edit Account */}
+          <Route path="/AboutUs" element={<AboutUs />} /> {/* Route cho AboutUs */}
+          <Route path="/Login" element={<Login />} /> {/* Route cho Login */}
+          <Route path="/Register" element={<Register />} /> {/* Route cho Register */}
+          <Route path="/Cart" element={<Cart />} /> {/* Route cho Cart */}
+
         </Routes>
         
         <Footer />
