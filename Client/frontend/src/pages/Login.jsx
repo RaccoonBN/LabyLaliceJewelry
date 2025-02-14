eec54d4c0d93267c8ad794e63ae49953c10f40dd
Nduy <168271459+gsggsfsasdsdasd@users.noreply.github.com>
2025-02-14 10:24:37 +0700

    Update Login.jsx

--- Client/frontend/src/pages/Login.jsx
+++ Client/frontend/src/pages/Login.jsx
@@ -22,27 +22,6 @@ const Login = () => {
             <button className="auth-button">Đăng Nhập</button>
           </div>
         </div>
-
-        {/* Đăng Ký */}
-        <div className="auth-section">
-          <h2 className="text_title_loginlogin">Đăng Ký</h2>
-          <div className="auth-form">
-            <div className="auth-input">
-              <FaUser className="input-icon" />
-              <input type="text" placeholder="Tên đăng nhập" />
-            </div>
-            <div className="auth-input">
-              <FaLock className="input-icon" />
-              <input type="password" placeholder="Mật khẩu" />
-            </div>
-            <div className="auth-input">
-              <FaLock className="input-icon" />
-              <input type="password" placeholder="Nhập lại mật khẩu" />
-            </div>
-            <p className="auth-link">Đã có tài khoản? Đăng nhập ngay</p>
-            <button className="auth-button">Đăng Ký</button>
-          </div>
-        </div>
       </div>
     </div>
   );
